5f28d856f953367beae884da423b0e51906098e6
Omewah <joelomewah@ymail.com>
2024-01-08 19:50:53 +0100

    a basic object list

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-export default function getListStudents() {
-    return [
-        {
-            id: 1,
-            firstName: 'Guillaume',
-            location: 'San Francisco',
-        },
-        {
-            id: 2,
-            firstName: 'James',
-            location: 'Columbia',
-        },
-        {
-            id: 5,
-            firstName: 'Serena',
-            location: 'San Francisco',
-        }
-    ];
-}
-=======
 export default function getListStudents() {
 	return [
 		{
@@ -38,4 +17,3 @@ export default function getListStudents() {
 		}
 	];
 }
->>>>>>> e8327e6c6195de226dd796e66375412378bbd0f3
