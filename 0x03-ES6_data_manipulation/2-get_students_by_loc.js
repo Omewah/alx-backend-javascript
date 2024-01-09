6ffb737208e88794d13d8025607e45a2076dd8c6
Omewah <joelomewah@ymail.com>
2024-01-09 08:25:40 +0100

    returns an array of objects who are located in a specific city

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,23 +1,3 @@
-export default function getListStudents() {
-  return [
-    {
-      id: 1,
-      firstName: 'Guillaume',
-      location: 'San Francisco',
-    },
-    {
-      id: 2,
-      firstName: 'James',
-      location: 'Columbia',
-    },
-    {
-      id: 5,
-      firstName: 'Serena',
-      location: 'San Francisco',
-    },
-  ];
-}
-
-export function getStudentsByLocation(students, city) {
+export default function getStudentsByLocation(students, city) {
   return students.filter((student) => student.location === city);
 }
